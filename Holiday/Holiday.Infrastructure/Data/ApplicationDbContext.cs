0623b372979cb2efa93a6d3de5e74deda99b6efc
Danielle van Zanden <159949437+dvzanden@users.noreply.github.com>
2026-06-16 11:02:47 +0200

    init 2

---
+++ Holiday/Holiday.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holiday.Infrastructure.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
